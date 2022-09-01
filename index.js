b9a36b3e51c3fffe9a1609708be3982b5620bdd4
Payal Sasmal <30835865+PayalSasmal10@users.noreply.github.com>
2022-09-01 18:27:36 +0530

    All files uploaded

---
+++ index.js
@@ -0,0 +1,75 @@
+// set the kgs and height values.
+let spans = document.querySelectorAll('.span_btn').length;
+
+for(var i = 0; i<spans; i++){
+
+    document.querySelectorAll('.span_btn')[i].addEventListener("click", function(){
+        var spanInnerHtml = this.innerHTML;
+        pressSpanBtn(spanInnerHtml);
+    } );
+}
+
+function pressSpanBtn(key){
+    let val = parseInt(document.querySelector('.count').value);
+    switch(key){
+        case "-":
+            val -= 1;
+            document.querySelector(".count").value = val;
+            break;
+        case "+":
+            val += 1;
+            document.querySelector(".count").value = val;
+            // console.log(document.querySelector(".count").value);
+            break;
+    }
+}
+
+// change the icon color
+let iconColor = document.querySelectorAll(".icon").length;
+for(var j=0; j<iconColor; j++){
+    document.querySelectorAll(".icon")[j].addEventListener("click", function(){
+        var selectSex = this.classList[4];
+        
+        chooseSex(selectSex);
+
+    })
+}
+
+function chooseSex(key){
+    const females = document.getElementById("fst_female");
+    const males = document.getElementById("fst_male");
+    switch(key){
+        case "female":
+            females.style.color = '#717fe0'; 
+            males.style.color = 'black'; 
+            break;
+        case "male":
+            males.style.color = '#717fe0'; 
+            females.style.color = 'black'; 
+            break;
+    }
+}
+
+
+// move the arrow function.
+
+document.querySelector(".btn-bmi").addEventListener("click", function(){
+    let heights = document.querySelector(".heigt").valueAsNumber;
+    let weight = document.querySelector(".count").valueAsNumber;
+    let bmi = (weight/(heights/100)**2).toFixed(1); // round the number.
+    document.querySelector(".result").value = bmi;
+    document.getElementById("result-id").style.visibility = "visible";
+    if (bmi <=18.4){
+        document.getElementById("arrow-icon").style.left = "430px";
+    }
+    else if( bmi >=18.5 && bmi <=25){
+        document.getElementById("arrow-icon").style.left = "650px";
+    }
+    else if(bmi > 25 && bmi <=30){
+        document.getElementById("arrow-icon").style.left = "870px";
+    }
+    else if( bmi>30 ){
+        document.getElementById("arrow-icon").style.left = "1100px";
+    }
+
+});
